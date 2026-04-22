6a8ac844404e09d5afefef53676b6b220184b9c8
Denys Ovcharov <denisovcharov88@gmail.com>
2026-04-22 22:42:05 +0200

    added start code

---
+++ src/server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import 'dotenv/config';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { errors } from 'celebrate';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+app.use(cors());
+app.use(logger);
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(notFoundHandler);
+
+app.use(errors());
+
+app.use(errorHandler);
+
+await connectMongoDB();
+
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on port ${PORT}`);
+});
