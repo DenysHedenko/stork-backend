a26125693c45d4e6b1974f000a5bae42d05dffe7
Max <yurchenko.maksym@gmail.com>
2026-04-23 12:14:24 +0100

    change taskRout server.js

--- src/server.js
+++ src/server.js
@@ -13,10 +13,11 @@ const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 app.use(cors());
-app.use(taskRoutes);
 app.use(logger);
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(taskRoutes);
 
 app.use(notFoundHandler);
 
